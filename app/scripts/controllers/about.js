0902ec1a7956f6bd0da52c4970ea36b6e88ce918
Julian Tiemann <Jutiemann@googlemail.com>
2015-08-31 12:22:06 +0200

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eMergencyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the eMergencyApp
+ */
+angular.module('eMergencyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eMergencyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eMergencyApp
+ */
+angular.module('eMergencyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
