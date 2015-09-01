0d2d29e480ba7d8479bc4e4094f7ca99aba6eb79
Jan-Hendrik Briese <jan@janbriese.de>
2015-09-01 16:45:01 +0200

    Included arunisrael/angularjs-geolocation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the eMergencyApp
  */
 angular.module('eMergencyApp')
-  .controller('MainCtrl', function ($scope, eventService) {
+  .controller('MainCtrl', function ($scope, eventService, geolocation) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -33,49 +33,10 @@ angular.module('eMergencyApp')
         console.log(error);
       });
 
-      $scope.lat = "0";
-      $scope.lng = "0";
-      $scope.accuracy = "0";
-      $scope.error = "";
-
-      $scope.showResult = function () {
-          return $scope.error == "";
-      }
-
-      $scope.showPosition = function (position) {
-          $scope.lat = position.coords.latitude;
-          $scope.lng = position.coords.longitude;
-          $scope.accuracy = position.coords.accuracy;
-          $scope.$apply();
-      }
-
-      $scope.showError = function (error) {
-          switch (error.code) {
-              case error.PERMISSION_DENIED:
-                  $scope.error = "User denied the request for Geolocation."
-                  break;
-              case error.POSITION_UNAVAILABLE:
-                  $scope.error = "Location information is unavailable."
-                  break;
-              case error.TIMEOUT:
-                  $scope.error = "The request to get user location timed out."
-                  break;
-              case error.UNKNOWN_ERROR:
-                  $scope.error = "An unknown error occurred."
-                  break;
-          }
-          $scope.$apply();
-      }
-
-      $scope.getLocation = function () {
-          if (navigator.geolocation) {
-              navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError);
-          }
-          else {
-              $scope.error = "Geolocation is not supported by this browser.";
-          }
-      }
-
-      $scope.getLocation();
+    $scope.coords = {};
+    geolocation.getLocation().then(function(data){
+      $scope.coords = {lat:data.coords.latitude, long:data.coords.longitude};
+      debugger
+    });
 
   });
