e9e2e606936a85742813f3fad3c01d0ac1cbde73
Jan-Hendrik Briese <jan@janbriese.de>
2015-09-09 11:22:23 +0200

    Ereignisse werden in Google Maps dargestellt, bei klick auf ein Ereignis wird ein InfoWindow gezeigt. Automatischer Push funktioniert noch nicht!

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,24 +18,51 @@ angular.module('eMergencyApp')
 
     eventService.all($scope.events)
       .then(function(response) {
-        $scope.events = response;
-
-        eventService.subscribe(function(e, event) {
-          $scope.events.push(event);
+        angular.forEach(response, function(entry) {
+          if(entry.location.latitude && entry.location.longitude) {
+            var event = {
+              id: entry.id,
+              latitude: entry.location.latitude,
+              longitude: entry.location.longitude,
+              icon: 'images/eventtype/' + entry.type.bezeichnung + '.png',
+              content: '<h3>' + entry.type.bezeichnung + '</h3>' + entry.additional,
+              show: false
+            };
+            event.onClick = function() {
+              event.show = !event.show;
+            }
+            $scope.events.push(event);
+          }
+        });
+        eventService.subscribe(function(e, entry) {
+          if(entry.location.latitude && entry.location.longitude) {
+            var event = {
+              id: entry.id,
+              latitude: entry.location.latitude,
+              longitude: entry.location.longitude,
+              icon: 'images/eventtype/' + entry.type.bezeichnung + '.png',
+              content: '<h3>' + entry.type.bezeichnung + '</h3>' + entry.additional,
+              show: false
+            };
+            event.onClick = function() {
+              event.show = !event.show;
+            }
+            $scope.events.push(event);
+          }
         });
 
       }, function(error) {
         console.log(error);
       });
 
-      $scope.coords = {};
-      geoLocService.get()
-        .then(function(location) {
-          $scope.coords = location
-          $scope.map = {center: {latitude: location.lat, longitude: location.long }, zoom: 14, options:  {scrollwheel: false}};
-        }, function(error) {
-            console.log(error);
-        });
-
+    $scope.coords = {};
+    $scope.map = {};
+    geoLocService.get()
+      .then(function(location) {
+        $scope.coords = location
+        $scope.map = {center: {latitude: location.lat, longitude: location.long}, zoom: 14, options:  {scrollwheel: false}};
+      }, function(error) {
+          console.log(error);
+      });
 
   });
