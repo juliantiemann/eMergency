74af09f051c57b0e2814b7c2a8568c93a4eb7965
Jan-Hendrik Briese <jan@janbriese.de>
2015-09-11 11:36:25 +0200

    Handwerker quickAdd integriert

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -9,9 +9,11 @@
  */
 angular.module('eMergencyApp')
   .controller('EventCtrl', function ($scope, eventService, geoLocService, userService) {
+    $scope.userName = userService.user.username;
+    
 
     $scope.setMyMarker = function () {
-      geoLocService.getStorage()
+      geoLocService.get()
         .then(function(location) {
           $scope.coords = location;
           $scope.map.myMarker = {
@@ -19,18 +21,24 @@ angular.module('eMergencyApp')
             latitude: location.lat,
             longitude: location.long
           };
+        }, function(error) {
+          console.log(error);
         });
     };
 
     $scope.quickEvent = function (type) {
-      debugger
-      var newEvent = {
-        type: eventService.eventTypes[type],
-        location: $scope.map.myMarker.latitude + ';' + $scope.map.myMarker.longitude,
-        additional: 'Quick Event! EMERGENCY!!!',
-        user: userService.user.id
-      };
-      debugger
-      eventService.add(newEvent);
+      if(type == 'handwerk') {
+        $scope.eventType = "handwerk";
+        $('#description').focus();
+      }
+      else {
+        var newEvent = {
+          type: eventService.eventTypes[type],
+          location: $scope.map.myMarker.latitude + ';' + $scope.map.myMarker.longitude,
+          additional: 'Quick Event! EMERGENCY!!!',
+          user: userService.user.id
+        };
+        eventService.add(newEvent);
+      }
     };
   });
