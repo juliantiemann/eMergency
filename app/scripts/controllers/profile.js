ec01bfea77bc90a52895f6f1999b24fc3ac9e39a
Jan-Hendrik Briese <jan@janbriese.de>
2015-09-16 11:00:36 +0200

    Update

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -8,6 +8,32 @@
  * Controller of the eMergencyApp
  */
 angular.module('eMergencyApp')
-  .controller('ProfileCtrl', function ($scope) {
-    var profile = {};
+  .controller('ProfileCtrl', function ($scope, userService, Notification) {
+    $scope.userService = userService;
+    $scope.user = {
+      name: userService.user.name,
+      username: userService.user.username,
+      Skills: userService.user.Skills
+    };
+
+
+    $scope.editUser = function() {
+      /*var newEvent = {
+        type: eventService.eventTypes[$scope.newEvent.type],
+        additional: $scope.newEvent.additional
+      };
+      eventService.add(newEvent)
+        .then(function() {
+          $scope.newEvent = {};
+          Notification.success('Alarm abgeschickt');
+        });
+      */
+      Notification.success('Profil bearbeitet');
+      $scope.userService = userService;
+    }
+
+    $scope.cancel = function() {
+      debugger
+      $scope.userService = userService;
+    }
   });
